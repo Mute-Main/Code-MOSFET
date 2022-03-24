ea47ffb2b2206560d6891213f79a73e4a9a32f25
Mute-Main <102311204+Mute-Main@users.noreply.github.com>
2022-03-24 16:38:28 +0100

    Add files via upload

---
+++ Stuurschakeling_met_MOSFET-transistor.ino
@@ -0,0 +1,28 @@
+const int frequentie = 1000;  // in Hz
+const int LichtMeter = A1;
+const int PotentioMeter = A2;
+const int LED_rood = 4;
+
+double lichtmeting;
+double schakeldrempel;
+
+
+void setup() {
+pinMode(LED_rood,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  
+  lichtmeting = analogRead(LichtMeter);
+  schakeldrempel = analogRead(PotentioMeter);
+  Serial.print(lichtmeting);
+  Serial.print("       ");
+  Serial.println(schakeldrempel);
+  
+  if (lichtmeting > schakeldrempel)
+    digitalWrite(LED_rood,HIGH);
+    
+  else
+    digitalWrite(LED_rood,LOW);
+}
