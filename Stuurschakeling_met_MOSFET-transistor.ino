1d45f4f99b461e65b93493b563186e1bf6856220
Bjarne <83133360+BjarneVerschorre@users.noreply.github.com>
2023-01-09 15:40:22 +0100

    Fixed syntax
    Thank me later B)

--- Stuurschakeling_met_MOSFET-transistor.ino
+++ Stuurschakeling_met_MOSFET-transistor.ino
@@ -1,27 +1,26 @@
-const int LichtMeter = A1;
-const int PotentioMeter = A2;
-const int LED_rood = 4;
+const int LICHT_METER = A1;
+const int POTENTIO_METER = A2;
+const int LED_ROOD = 4;
 
 double lichtmeting;
 double schakeldrempel;
 
-
 void setup() {
-pinMode(LED_rood,OUTPUT);
-Serial.begin(9600);
+  pinMode(LED_ROOD,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-  lichtmeting = analogRead(LichtMeter);
-  schakeldrempel = analogRead(PotentioMeter);
+  lichtmeting = analogRead(LICHT_METER);
+  schakeldrempel = analogRead(POTENTIO_METER);
+
   Serial.print(lichtmeting);
-  Serial.print("       ");
+  Serial.print("\t");
   Serial.println(schakeldrempel);
   
   if (lichtmeting > schakeldrempel)
-    digitalWrite(LED_rood,HIGH);
+    digitalWrite(LED_ROOD,HIGH);
     
   else
-    digitalWrite(LED_rood,LOW);
+    digitalWrite(LED_ROOD,LOW);
 }
